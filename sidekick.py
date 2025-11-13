82525f0566d175f18e50b9f06d771c33843c463c
Hooopla <Johngabrielbello@outlook.com>
2025-11-13 14:53:40 -0800

    starter for sidekick.py

---
+++ sidekick.py
@@ -0,0 +1,31 @@
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+from langgraph.prebuilt import ToolNode
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from typing import List, Any, Optional, Dict
+from pydantic import BaseModel, Field
+from sidekick_tools import playwright_tools, other_tools
+import uuid
+import asyncio
+from datetime import datetime
+
+load_dotenv(override=True)
+
+class State(TypedDict):
+  messages: Annotated[List[any], add_messages]
+  success_criteria: str
+  feedback_on_work: Optional[str]
+  success_criteria_met: bool
+  user_input_needed: bool
+
+class EvaluatorOutput(BaseModel):
+  feedback: str = Field(description="Feedback on the assistant's response")
+  success_criteria_met: bool = Field(description="Whether the success criteria have been met")
+  user_input_needed: bool = Field(description="True if more input is needed from the user, or clarifications, or the assistant is stuck")
+
+class Sidekick:
