96901e401f225ccad37aeb2a71e15c925696ea37
Hooopla <Johngabrielbello@outlook.com>
2025-11-13 16:17:39 -0800

    Evaluator & router

--- sidekick.py
+++ sidekick.py
@@ -103,7 +103,7 @@ class Sidekick:
     else:
       return "evaluator"
 
-  # Format Conversation
+  # Format Conversation | Used for conversation history to pass
   def format_conversation(self, messages: List[Any]) -> str:
     conversation = "Conversation history:\n\n"
     for message in messages:
@@ -115,9 +115,60 @@ class Sidekick:
     return conversation
 
   # Evaluator
-  
+  def evaluator(self, state: State) -> State:
+    last_response = state["messages"][-1].content
+
+    system_message = """You are an evaluator that determines if a task has been completed successfully by an Assistant.
+    Assess the Assistant's last response based on the given criteria. Respond with your feedback, and with your decision on whether the success criteria has been met,
+    and whether more input is needed from the user."""
+
+    user_message = f"""You are evaluating a conversation between the User and Assistant. You decide what action to take based on the last response from the Assistant.
+
+    The entire conversation with the assistant, with the user's original request and all replies, is:
+    {self.format_conversation(state["messages"])}
+
+    The success criteria for this assignment is:
+    {state["success_criteria"]}
+
+    And the final response from the Assistant that you are evaluating is:
+    {last_response}
+
+    Respond with your feedback, and decide if the success criteria is met by this response.
+    Also, decide if more user input is required, either because the assistant has a question, needs clarification, or seems to be stuck and unable to answer without help.
+
+    The Assistant has access to a tool to write files. If the Assistant says they have written a file, then you can assume they have done so.
+    Overall you should give the Assistant the benefit of the doubt if they say they've done something. But you should reject if you feel that more work should go into this.
+
+    """
+    if state["feedback_on_work"]:
+      user_message += f"Also, note that in a prior attempt from the Assistant, you provided this feedback: {state['feedback_on_work']}\n"
+      user_message += "If you're seeing the Assistant repeating the same mistakes, then consider responding that user input is required."
+
+    evaluator_messages = [
+      SystemMessage(content=system_message),
+      HumanMessage(content=user_message),
+      ]
+
+    eval_result = self.evaluator_llm_with_output.invoke(evaluator_messages)
+    new_state = {
+      "messages": [
+        {
+        "role": "assistant",
+        "content": f"Evaluator Feedback on this answer: {eval_result.feedback}",
+        }
+      ],
+      "feedback_on_work": eval_result.feedback,
+      "success_criteria_met": eval_result.success_criteria_met,
+      "user_input_needed": eval_result.user_input_needed,
+    }
+    return new_state
 
   # Evaluator-Router
+  def route_based_on_evaluation(self, state:State)-> str:
+    if state["success_criteria_met"] or state["user_input_needed"]:
+      return "END"
+    else:
+      return "worker"
 
   # Build Graph
 
